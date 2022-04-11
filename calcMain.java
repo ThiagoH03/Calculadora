3b4169c219fa155925390a976b882a903cb8613e
ThiagoH03 <thiagohenriq03@outlook.com>
2022-04-11 19:34:00 +0000

    ...

---
+++ calcMain.java
@@ -0,0 +1,10 @@
+public class calcMain{
+    public static void main(String[] args){
+        calcSoma Calculadora = new calcSoma();
+
+        System.out.println(Calculadora.soma(15,5));
+        System.out.println(Calculadora.soma(1.3,2.7));
+        System.out.println(Calculadora.soma(12,3,21));
+        
+    }
+}
